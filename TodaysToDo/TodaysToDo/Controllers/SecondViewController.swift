7f9e24ba3c3e6a6f9b11cbca145c761f11ca0e44
naipaka <naipakapaka@gmail.com>
2019-04-11 21:05:00 +0900

    デバッグ用コードの削除

--- TodaysToDo/TodaysToDo/Controllers/SecondViewController.swift
+++ TodaysToDo/TodaysToDo/Controllers/SecondViewController.swift
@@ -24,7 +24,6 @@ class SecondViewController: UIViewController , UITableViewDelegate, UITableViewD
         // Realmからデータを取得
         do {
             let realm = try Realm()
-            print(Realm.Configuration.defaultConfiguration.fileURL!)
             let predicate = NSPredicate(format: "%@ =< startDateTime AND startDateTime < %@", getBeginingAndEndOfToday().beginingOfToday as CVarArg, getBeginingAndEndOfToday().endOfToday as CVarArg)
             todaysTodoList = realm.objects(ToDo.self).filter(predicate).sorted(byKeyPath: "startDateTime")
         } catch {
