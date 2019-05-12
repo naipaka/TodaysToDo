a289ceb3c739fa479485f4cd159923987c549d24
naipaka <naipakapaka@gmail.com>
2019-05-12 17:16:29 +0900

    チュートリアルの画面をfix1.0.2に合わせて修正

--- TodaysToDo/TodaysToDo/Controllers/TutorialViewControllers/TutorialFirstViewController.swift
+++ TodaysToDo/TodaysToDo/Controllers/TutorialViewControllers/TutorialFirstViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class TutorialFirstViewController: UIViewController {
 
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var skipButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +21,35 @@ class TutorialFirstViewController: UIViewController {
         descriptionLabel.numberOfLines = 10
         //contentsのサイズに合わせてobujectのサイズを変える
         descriptionLabel.sizeToFit()
+        
+        skipButton.layer.cornerRadius = 10
+    }
+    
+    @IBAction func skip(_ sender: UIButton) {
+        dispCloseTutorialAlert()
+    }
+    
+    // 日付を設定できない時のアラート表示メソッド
+    func dispCloseTutorialAlert() {
+        let alertController:UIAlertController = UIAlertController(
+            title: "チュートリアルを終了しますか？",
+            message: "チュートリアルは設定からいつでも見返せます。",
+            preferredStyle:  UIAlertController.Style.alert
+        )
+        
+        let okAction: UIAlertAction = UIAlertAction(title: "OK！", style: UIAlertAction.Style.default) { (action: UIAlertAction) in
+            // 画面を閉じる
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel) { (action: UIAlertAction) in
+            // 設定をキャンセルする
+            return
+        }
+        
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
 }
